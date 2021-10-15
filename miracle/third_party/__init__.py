efb187c4e61d43770ccd259228fa0767a7d6a419
Bogdan Cebere <bogdan.cebere@gmail.com>
2021-10-15 13:57:47 +0300

    add third party imputers

---
+++ miracle/third_party/__init__.py
@@ -0,0 +1,6 @@
+from . import imputation_gain  # noqa: F401
+from . import imputation_knn  # noqa: F401
+from . import imputation_mean  # noqa: F401
+from . import imputation_mice  # noqa: F401
+from . import imputation_missforest  # noqa: F401
+from . import imputation_ot  # noqa: F401
